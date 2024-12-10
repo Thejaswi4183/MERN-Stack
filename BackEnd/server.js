86fe82abf2522a699bbbd93c5e55e78440a4895d
Thejaswi S <thejaswi4uns@gmail.com>
2024-12-10 21:23:13 +0530

    Added db Connection

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -21,13 +21,14 @@ app.use("/api/workouts", workoutRoutes);
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    
+    //listen for requests
+app.listen(process.env.PORT, () => {
+  console.log("Connected to db and Listening on port", process.env.PORT);
+});
+
   })
   .catch((error) => {
     console.log(error);
   });
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-  console.log("Listening on port", process.env.PORT);
-});
+
