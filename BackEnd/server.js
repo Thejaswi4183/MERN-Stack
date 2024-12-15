eb04be9c6d0b82a5db698749d23d6f698e37db58
Thejaswi S <thejaswi4uns@gmail.com>
2024-12-15 22:26:35 +0530

    Modified

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,10 +1,18 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
+const cors = require("cors");
 const express = require("express");
 const workoutRoutes = require("./routes/workouts");
 
 //express app
 const app = express();
+
+//cors
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 //middleware
 app.use(express.json());
@@ -22,13 +30,10 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     //listen for requests
-app.listen(process.env.PORT, () => {
-  console.log("Connected to db and Listening on port", process.env.PORT);
-});
-
+    app.listen(process.env.PORT, () => {
+      console.log("Connected to db and Listening on port", process.env.PORT);
+    });
   })
   .catch((error) => {
     console.log(error);
   });
-
-
